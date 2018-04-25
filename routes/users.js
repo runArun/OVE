c8f9a3354838219d877d85a66d18749e5be60eec
runArun <zl2613185@qq.com>
2018-04-25 16:04:19 +1000

    video.js

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/login', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/signup', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signup', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
